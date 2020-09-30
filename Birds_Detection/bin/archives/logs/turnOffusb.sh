fd61ca61877fb8cb14d802aaf36180eee2a3b19a
MarcPozzo <marc.pozzo@gmail.com>
2020-09-30 14:05:48 +0200

    deleting of code Folder

--- Birds_Detection/bin/archives/logs/turnOffusb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# turn off usb (saves 50-100mA), also unplug the etherne cable or 10/20mA remain
-# should be run as sudo 
-# doesn't shut down the wifi, only the ethernet
-sudo service networking stop
-echo 0 > /sys/devices/platform/soc/3f980000.usb/buspower
-
--- Birds_Detection/bin/archives/logs/turnOnusb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# turn on usb after ~/bin/turnOffusb.sh turned it off
-# should be run as sudo 
-echo 1 > /sys/devices/platform/soc/3f980000.usb/buspower
-sudo service networking start
-
