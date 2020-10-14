2448402a987d94472bb037672cec1817fbeea2b2
MarcPozzo <marc.pozzo@gmail.com>
2020-10-14 12:04:05 +0200

    propose interractive input to select the option you want

--- Birds_Detection/Train/Lenet/4_Chanels_test/Base Constructor/get_4C_chanel.py
+++ Birds_Detection/Train/Lenet/4_Chanels_test/Base Constructor/get_4C_chanel.py
@@ -6,6 +6,10 @@ Created on Wed Oct 14 10:30:46 2020
 @author: marcpozzo
 """
 
+
+
+
+#Import libraries
 import pandas as pd
 import functions_4C as fn
 import time
@@ -13,33 +17,48 @@ from sklearn.model_selection import train_test_split
 import gc
 from numpy import save
 
+Mat_path="../../Materiels/"
 
-test_size=0.2
-path="/mnt/VegaSlowDataDisk/c3po/Images_aquises/"
-type_of_diff="GBR"
-
-
-
+path="/mnt/VegaSlowDataDisk/c3po/Images_aquises/"#Where you pictures are
 liste_folders=['./DonneesPI/timeLapsePhotos_Pi1_0','./DonneesPI/timeLapsePhotos_Pi1_1','./DonneesPI/timeLapsePhotos_Pi1_2','./DonneesPI/timeLapsePhotos_Pi1_3','./DonneesPI/timeLapsePhotos_Pi1_4']
 
-df=pd.read_csv(path+"images_labels.csv")
-df=fn.open_table(df,liste_folders)
 
-liste_name_test,filename_liste=list(fn.get_liste_name_test(df))
-df["filename"]=list(fn.get_liste_name_test(df))[0]
+print("The 4 chanels correspond to the difference between an image and the image just before. In sense add this 4TH chanels could be considered as to add a temporel chanels.")
+print("the difference between the 2 pictures is made in grey colors.")
+print("If you want to apply the conversion of images to gray and the difference that follows type HSV.")
+print("If you want to apply filters before converting to gray, type GBR.")
+type_of_diff=input()
+print("Type train or test wethever you want to rec picture with a fourth chanel for train or test sample")
+print( "I advise you to beguin with small proportion to be sure that your computer has enough memory get pictures")
+Sample=input(" Please type train or test " )
+test_size = input("Type a number between 0.1 and 0.5 to indicate the proportion of data you want put in test sample : ")
 
 
-base_train,base_test= train_test_split(df,stratify=df["classe"], test_size=test_size,random_state=42)
-del df
+
+
+
+imagettes=pd.read_csv(Mat_path+"imagettes.csv")
+imagettes=fn.open_table(imagettes,liste_folders)
+liste_name_test,filename_liste=list(fn.get_liste_name_test(imagettes))
+imagettes["filename"]=list(fn.get_liste_name_test(imagettes))[0]
+
+
+base_train,base_test= train_test_split(imagettes,stratify=imagettes["classe"], test_size=test_size,random_state=42)
+if Sample=="train":
+    base=base_train
+    del imagettes,base_test
+elif Sample=="test":
+    base=base_test
+    del imagettes,base_train  
 gc.collect()
 
 
+
+
 start=time.time()
-X_train,Y_train,base_train_trans=fn.get_X_Y_bis(base_train,diff=type_of_diff)
+X,Y_train,base_train_trans=fn.get_X_Y(base,diff=type_of_diff)
 end=time.time()
-
 print(end-start)
-save('imagettes4C_train.npy', X_train)
-
+save('imagettes4C_'+type_of_diff+'_'+Sample +'.npy', X)
 gc.collect()
 
